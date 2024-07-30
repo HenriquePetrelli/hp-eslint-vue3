fe9174d2b9dc88469aa4096d31f98f875a95234a
Henrique Petrelli <henriquepetrelli96@gmail.com>
2024-07-29 23:20:08 -0300

    feat: adicionada regra

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 module.exports = {
-    root: true,
-    env: {
-      browser: true,
-      node: true,
-      es2021: true
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:vue/vue3-recommended',
-      '@vue/eslint-config-standard'
-    ],
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module'
-    },
-    rules: {
-    }
-  };
-  
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    'vue/setup-compiler-macros': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-standard',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  rules: {
+    'no-undef': 'off',
+    '@typescript-eslint/no-undef': 'off',
+  },
+};
